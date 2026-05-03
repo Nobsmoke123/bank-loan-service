c7017623b02467872e2a8679ba68ce8481f4e9ee
Nobsmoke123 <donaldakobundu@gmail.com>
2026-05-03 22:33:57 +0100

    Enhance caching in LoansModule by integrating Redis and memory caching. Update .env.sample to include new Redis configuration options. Add dependencies for cache management and Redis support in package.json and pnpm-lock.yaml.

--- src/loans/loans.module.ts
+++ src/loans/loans.module.ts
@@ -2,9 +2,32 @@ import { Module } from '@nestjs/common';
 import { LoansService } from './loans.service';
 import { LoansController } from './loans.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { KeyvCacheableMemory } from 'cacheable';
+import { Keyv } from 'keyv';
+import KeyvRedis from '@keyv/redis';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        ttl: configService.get<number>('REDIS_TTL')!,
+        stores: [
+          new Keyv({
+            store: new KeyvCacheableMemory({
+              lruSize: configService.get<number>('REDIS_LRU_SIZE')!,
+            }),
+          }),
+
+          new KeyvRedis(configService.get<string>('REDIS_URL')!),
+        ],
+      }),
+    }),
+  ],
   providers: [LoansService],
   controllers: [LoansController],
 })
